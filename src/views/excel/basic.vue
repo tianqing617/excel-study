fe4c244626448c3738e195267032b29b98e0bedc
刘天青 <liutianqing@dfsoft.com.cn>
2020-05-26 18:22:15 +0800

    feat: fill cotent

--- src/views/excel/basic.vue
+++ src/views/excel/basic.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="basic">
-    <button @click="createExcel">create excel</button>
+    <button style="width: 200px; height: 50px" @click="createExcel">create excel</button>
   </div>
 </template>
 
@@ -17,7 +17,7 @@ export default {
     createExcel() {
       // 1. 初始化excel
       const workbook = new ExcelJS.Workbook();
-      console.log(workbook);
+      // console.log(workbook);
       workbook.creator = 'tianqing';
 
       // 2. 创建视图
@@ -31,14 +31,35 @@ export default {
         visibility: 'visible',
       }]
 
-      workbook.addWorksheet('my sheet');
+      const mySheet = workbook.addWorksheet('my sheet');
 
       // 2. 生成内容
+      mySheet.columns = [
+        { header: '编号', key: 'id', width: 10 },
+        { header: '姓名', key: 'name', width: 32 },
+      ];
+
+      // const idCol = mySheet.getColumn('id');
+      // const nameCol = mySheet.getColumn('name');
+
+      // idCol.values = [1, 2, 4];
+      // nameCol.values = ['jim', 'tom', 'lucy'];
+
+      mySheet.addRow({ id: 1, name: 'John Doe' });
+      mySheet.addRow([2, 'tom']);
+
+      const myRow = mySheet.getRow(2);
+      myRow.getCell(2).value = 'tianqing'; // B2's value set to tianqing
+
+      const myRow4 = mySheet.getRow(4);
+      myRow4.values = {
+        id: 13,
+        name: 'Thing 1',
+      };
 
       // 3. 输出文件
       workbook.xlsx.writeBuffer().then(data => {
         const blob = new Blob([data]);
-
         saveAs(blob, 'text.xlsx');
       });
     },
