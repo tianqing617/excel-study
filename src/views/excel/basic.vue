5268e972b4a5f5474b68a338ca39dbeef28494e3
刘天青 <liutianqing@dfsoft.com.cn>
2020-05-26 17:22:16 +0800

    feat: download excel file

--- src/views/excel/basic.vue
+++ src/views/excel/basic.vue
@@ -1,3 +1,34 @@
 <template>
-  <span>basic excel</span>
+  <div class="basic">
+    <button @click="createExcel">create excel</button>
+  </div>
 </template>
+
+<script>
+import ExcelJS from 'exceljs';
+import { saveAs } from 'file-saver';
+
+export default {
+  name: 'BasicExcel',
+  mounted() {
+    console.log('exceljs', ExcelJS);
+  },
+  methods: {
+    createExcel() {
+      // 1. 初始化excel
+      const workbook = new ExcelJS.Workbook();
+      console.log(workbook);
+      workbook.creator = 'tianqing';
+
+      // 2. 生成内容
+
+      // 3. 输出文件
+      workbook.xlsx.writeBuffer().then(data => {
+        const blob = new Blob([data]);
+        
+        saveAs(blob, 'text.xlsx');
+      });
+    },
+  },
+}
+</script>
