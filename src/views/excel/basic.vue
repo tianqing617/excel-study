f896855c86f24c392d518f94e4743c89a88c3158
刘天青 <liutianqing@dfsoft.com.cn>
2020-05-26 17:28:40 +0800

    feat: generate excel file

--- src/views/excel/basic.vue
+++ src/views/excel/basic.vue
@@ -20,12 +20,25 @@ export default {
       console.log(workbook);
       workbook.creator = 'tianqing';
 
+      // 2. 创建视图
+      workbook.views = [{
+        x: 0,
+        y: 0,
+        width: 10000,
+        height: 20000,
+        firstSheet: 0,
+        activeTab: 1,
+        visibility: 'visible',
+      }]
+
+      workbook.addWorksheet('my sheet');
+
       // 2. 生成内容
 
       // 3. 输出文件
       workbook.xlsx.writeBuffer().then(data => {
         const blob = new Blob([data]);
-        
+
         saveAs(blob, 'text.xlsx');
       });
     },
