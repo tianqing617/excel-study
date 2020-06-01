e565be58ed55520da9573c6393dcc375c432c9a7
刘天青 <liutianqing@dfsoft.com.cn>
2020-06-01 13:56:03 +0800

    feat: navigator

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/excel/index">Index</router-link> |
+      <router-link to="/excel/basic">Basic</router-link>
     </div>
     <router-view/>
   </div>
